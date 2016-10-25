b35044f5f62c6bbd68d06784097fce12eaf99f09
kxu <kxu@freewheel.tv>
2016-10-26 01:49:43 +0800

    fix UI issue with semantic UI

--- mjcounter.js
+++ mjcounter.js
@@ -18,32 +18,23 @@ $().ready(function() {
         },
         computed: {
             newRound: function() {
-                let initData = [
+                return [
                     this.newRound_1,
                     this.newRound_2,
                     this.newRound_3,
                     this.newRound_4
                 ];
-                var finalData = initData.map((x) => {
-                    return parseInt(x.trim());
-                });
-                return finalData;
             },
             sumData: function() {
-                console.log('process sumData ...');
                 var sumData = [0, 0, 0, 0];
-                console.log(this.rounds);
                 this.rounds.map((x) => {
                     console.log(x);
                     sumData = sumData.map((s, index) => (s + x[index]));
                 });
-                console.log(sumData);
                 return sumData;
             },
             sumPercentage: function() {
-                console.log('process sumPercentage ...');
                 var sumPer = this.sumData;
-                console.log(sumPer);
                 if (!arraysEqual(sumPer, [0, 0, 0, 0])) {
                     console.log('Not Equal to!!')
                     var minValue = Math.min.apply(null, sumPer); // might have a few of them
@@ -57,14 +48,10 @@ $().ready(function() {
                         sumAll += x;
                         return x;
                     });
-                    console.log(minValue);
-                    console.log(sumAll);
-                    console.log(newArray);
-                    var tt = newArray.map((x) => {
+                    var finalPer = newArray.map((x) => {
                         return ((x / sumAll) * 100).toFixed(2);
                     });
-                    console.log(tt);
-                    return tt;
+                    return finalPer;
                 } else {
                     return [0, 0, 0, 0];
                 }
@@ -72,8 +59,6 @@ $().ready(function() {
         },
         methods: {
             addNewRound: function() {
-                console.log('process addNewRound ...');
-                console.log(this.newRound);
                 this.rounds.push(this.newRound);
                 this.newRound_1 = '';
                 this.newRound_2 = '';
@@ -91,10 +76,6 @@ function arraysEqual(a, b) {
     if (a === b) return true;
     if (a == null || b == null) return false;
     if (a.length != b.length) return false;
-
-    // If you don't care about the order of the elements inside
-    // the array, you should sort both arrays here.
-
     for (var i = 0; i < a.length; ++i) {
         if (a[i] !== b[i]) return false;
     }
