44eddf011592c34d00e867951ffddc0b8d65c878
kxu <kxu@freewheel.tv>
2016-10-26 18:26:34 +0800

    try type=tel

--- mjcounter.js
+++ mjcounter.js
@@ -19,22 +19,25 @@ $().ready(function() {
         },
         computed: {
             newRound: function() {
-                // let initData = [
-                //     this.newRound_1,
-                //     this.newRound_2,
-                //     this.newRound_3,
-                //     this.newRound_4
-                // ];
-                // var finalData = initData.map((x) => {
-                //     return parseInt(x.trim());
-                // });
-                // return finalData;
-                return [
+                let initData = [
                     this.newRound_1,
                     this.newRound_2,
                     this.newRound_3,
                     this.newRound_4
-                ]; // if use <input type="number">
+                ];
+                var finalData = initData.map((x) => {
+                    if (x == '')
+                        return 0;
+                    else
+                        return parseInt(x.trim());
+                });
+                return finalData;
+                // return [
+                //     this.newRound_1,
+                //     this.newRound_2,
+                //     this.newRound_3,
+                //     this.newRound_4
+                // ]; // if use <input type="number">
             },
             sumData: function() {
                 var sumData = [0, 0, 0, 0];
